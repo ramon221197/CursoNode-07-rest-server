8fd5610b3ef31cca86bece6a7227b3fdf6b5fd96
Ramon Garcia <ramon.garcia@ideappz.com>
2022-07-01 00:32:37 -0500

    verificar token de google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,73 +1,78 @@
 //Importacion de paquetes
-const { request, response } = require("express");
-const bcryptjs = require('bcryptjs');
+const { request, response, json } = require("express");
+const bcryptjs = require("bcryptjs");
 
-const Usuario = require('../models/usuario');
+const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/generarJWT");
+const { googleverify } = require("../helpers/google-verify");
 
+const login = async (req = request, resp = response) => {
+  const { correo, password } = req.body;
 
-
-
-
-const login = async(req = request, resp = response) => {
-
-    const { correo, password} = req.body;
-
-    try {
-
-        //verificar si el email existe
-        const usuario = await Usuario.findOne({ correo });
-        if ( !usuario ) { //si no existe el usuario
-            return resp.status(400).json({
-                msg: 'Usuario / Password no son correctos - correo'
-            });
-        }
-
-        //Verificar que el usuario esta activo
-        if ( !usuario.estado ) { //si no existe usuario.estado (estado: false)
-            return resp.status(400).json({
-                msg: 'Usuario / Password no son correctos - estado: false'
-            });
-        }
-
-        //verificar la contraseña
-        const validaPasswd = bcryptjs.compareSync(password, usuario.password); //compareSync compara el correo que se recibe en el req.body (1er argumento) y lo compara con el paaswd del usuario (2do argumento) deben hacer match
-        if( !validaPasswd ){
-            return resp.status(400).json({
-                msg: 'Usuario / Password no son correctos - password'
-            });
-        }
-
-        //Generar el JWT
-        const token = await generarJWT( usuario.id );
-        
-        resp.json({
-            msg: 'login ok',
-            usuario,
-            token
-        })
-
-    } catch (error) {
-        console.log(error);
-
-        resp.status(500).json({
-            msg: 'Hable con el administrador'
-        });
+  try {
+    //verificar si el email existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      //si no existe el usuario
+      return resp.status(400).json({
+        msg: "Usuario / Password no son correctos - correo",
+      });
     }
 
-}
+    //Verificar que el usuario esta activo
+    if (!usuario.estado) {
+      //si no existe usuario.estado (estado: false)
+      return resp.status(400).json({
+        msg: "Usuario / Password no son correctos - estado: false",
+      });
+    }
 
-const googleSingIn = async( req = request, res = response) => {
+    //verificar la contraseña
+    const validaPasswd = bcryptjs.compareSync(password, usuario.password); //compareSync compara el correo que se recibe en el req.body (1er argumento) y lo compara con el paaswd del usuario (2do argumento) deben hacer match
+    if (!validaPasswd) {
+      return resp.status(400).json({
+        msg: "Usuario / Password no son correctos - password",
+      });
+    }
+
+    //Generar el JWT
+    const token = await generarJWT(usuario.id);
+
+    resp.json({
+      msg: "login ok",
+      usuario,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+
+    resp.status(500).json({
+      msg: "Hable con el administrador",
+    });
+  }
+};
+
+const googleSingIn = async (req = request, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const googleUser = await googleverify(id_token);
+    console.log(googleUser);
     
-    const {id_token} = req.body;
 
     res.json({
-        msg: 'Todo bien',
-        id_token
+      msg: "Todo bien! google SigIn",
+      id_token,
+    });
+  } catch (error) {
+    json().status(400).json({
+        ok: false,
+        msg: 'El token no se pudo verificar'
     })
-}
+  }
+};
 
 module.exports = {
-    login,
-    googleSingIn
-}
+  login,
+  googleSingIn,
+};
