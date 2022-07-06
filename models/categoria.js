8432c598c755594ace47f29b598d4f459be9427b
Ramon Garcia <ramon.garcia@ideappz.com>
2022-07-05 21:31:54 -0500

    Modelo de Categorias

---
+++ models/categoria.js
@@ -0,0 +1,25 @@
+
+const {Schema, model} = require('mongoose');
+
+const CategoriaSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es requerido']
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: true
+    }
+
+});
+
+
+//Utilizamos la funcion "model" de Mongoose para exportar el modelo creado
+module.exports = model('Categoria', CategoriaSchema);
+//la cadena 'Usuario' es como se va a nombrar al modelo.
