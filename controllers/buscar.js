dbec296bffa91f3908887f9e3b3068b90f7fbeeb
Ramon Garcia <ramon.garcia@ideappz.com>
2022-07-07 00:37:53 -0500

    Buscar en otras colecciones

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -34,10 +34,58 @@ const buscarUsuarios = async( termino = '', resp = response) => {
         $or: [ {nombre: regexp}, {correo: regexp} ], // donde encuentre el nombre/correo sea = al termino
         $and: [{ estado: true}] //Y ademas debe estar activo
     }); // donde encuentre el nombre se = al termino
-    
+
     return resp.json({ //IMPORTANTE: poner el return para que no siga ejecutando nda de la funcion
         results: usuarios
     });
+}
+
+const buscarCategorias = async( termino = '', resp = response) => {
+
+    //BUSQUEDA POR ID
+    //verificacion para saber si el termino es un MongoID valido
+    const esMongoId = ObjectId.isValid( termino ); //TRUE
+
+    if ( esMongoId ) {
+        const categoria = await Categoria.findById( termino );
+        return resp.json({ //IMPORTANTE: poner el return para que no siga ejecutando nda de la funcion
+            results: (categoria) ? [ categoria ] : [ ] //si el usuario existe regresa el arreglo con lel usuario encontrado, si no regresa un arreglo vacio
+        });
+    }
+
+    //BUSQUEDA POR NOMBRE O CORREO
+    const regexp = new RegExp( termino , 'i'); //expresion regular para hacer insencible a mayusculas y minusculas la busqueda
+
+    const categorias = await Categoria.find({ nombre: regexp, estado: true }); //que encuentre por nombre y que el estado: true
+    return resp.json({ //IMPORTANTE: poner el return para que no siga ejecutando nda de la funcion
+        results: categorias
+    });
+
+}
+
+const buscarProductos = async( termino = '', resp = response) => {
+
+    //BUSQUEDA POR ID
+    //verificacion para saber si el termino es un MongoID valido
+    const esMongoId = ObjectId.isValid( termino ); //TRUE
+
+    if ( esMongoId ) {
+        const productos = await Producto.findById( termino ).
+                        populate('categoria','nombre');
+        return resp.json({ //IMPORTANTE: poner el return para que no siga ejecutando nda de la funcion
+            results: (productos) ? [ productos ] : [ ] //si el usuario existe regresa el arreglo con lel usuario encontrado, si no regresa un arreglo vacio
+        });
+    }
+
+    //BUSQUEDA POR NOMBRE 
+    const regexp = new RegExp( termino , 'i'); //expresion regular para hacer insencible a mayusculas y minusculas la busqueda
+
+    const productos = await Producto.find({ nombre: regexp, estado: true })//que encuentre por nombre y que el estado: true
+                    .populate('categoria','nombre'); 
+    return resp.json({ //IMPORTANTE: poner el return para que no siga ejecutando nda de la funcion
+        results: productos
+    });
+
 }
 
 const buscar = (req, resp = response) => {
@@ -58,9 +106,11 @@ const buscar = (req, resp = response) => {
 
         break;
         case 'categorias':
+            buscarCategorias(termino, resp);
             
         break;
         case 'productos':
+            buscarProductos(termino, resp);
             
         break;
 
