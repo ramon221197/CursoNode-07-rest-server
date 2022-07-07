fb98e84c8799770c69f83a7ec89c5917eac0637b
Ramon Garcia <ramon.garcia@ideappz.com>
2022-07-06 22:18:48 -0500

    Busquedas en Bases de datos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,10 +1,62 @@
+//IMPORTACIONES
 const { response } = require("express");
+const { ObjectId } = require('mongoose').Types;
+
+const { Usuario, Categoria, Producto } = require("../models"); //Importacion de los modelos
+
+
+
+//colecciones permitidad para las busquedas
+const colecccionesPermitidas = [
+    'usuarios',
+    'categorias',
+    'productos',
+    'roles',
+];
+
+const buscarUsuarios = async( termino = '', resp = response) => {
+    
+    //verificacion para saber si el termino es un MongoID valido
+    const esMongoId = ObjectId.isValid( termino ); //TRUE
+
+    if ( esMongoId ) {
+        const usuario = await Usuario.findById( termino );
+        resp.json({
+            results: (usuario) ? [ usuario ] : [ ] //si el usuario existe regresa el arreglo con lel usuario encontrado, si no regresa un arreglo vacio
+        })
+    }
+}
 
 const buscar = (req, resp = response) => {
 
-    resp.json({
-        msg: 'Buscar...'
-    });
+    const {coleccion, termino } = req.params;
+
+    //validar si la coleccion que se esta mandando en la url se encuentra en las colecciones permitidas
+    if ( !colecccionesPermitidas.includes( coleccion )) {
+        return resp.status(400).json({
+            msg:  `Las colecciones permitidas son: ${colecccionesPermitidas}`
+        })
+        
+    }
+
+    switch (coleccion) {
+        case 'usuarios':
+            buscarUsuarios(termino, resp);
+
+        break;
+        case 'categorias':
+            
+        break;
+        case 'productos':
+            
+        break;
+
+        default:
+            resp.status(500).json({
+                msg: 'Se me olvido hacer sta busqueda'
+            })
+
+    }
 
 }
 
