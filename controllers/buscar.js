7bd9048bef6188c00deb4240d890b9e90045eb5c
Ramon Garcia <ramon.garcia@ideappz.com>
2022-07-06 23:08:38 -0500

    Buscar con otros argumentos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -16,15 +16,28 @@ const colecccionesPermitidas = [
 
 const buscarUsuarios = async( termino = '', resp = response) => {
     
+    //BUSQUEDA POR ID
     //verificacion para saber si el termino es un MongoID valido
     const esMongoId = ObjectId.isValid( termino ); //TRUE
 
     if ( esMongoId ) {
         const usuario = await Usuario.findById( termino );
-        resp.json({
+        return resp.json({ //IMPORTANTE: poner el return para que no siga ejecutando nda de la funcion
             results: (usuario) ? [ usuario ] : [ ] //si el usuario existe regresa el arreglo con lel usuario encontrado, si no regresa un arreglo vacio
-        })
+        });
     }
+
+    //BUSQUEDA POR NOMBRE O CORREO
+    const regexp = new RegExp( termino , 'i'); //expresion regular para hacer insencible a mayusculas y minusculas la busqueda
+
+    const usuarios = await Usuario.find({ 
+        $or: [ {nombre: regexp}, {correo: regexp} ], // donde encuentre el nombre/correo sea = al termino
+        $and: [{ estado: true}] //Y ademas debe estar activo
+    }); // donde encuentre el nombre se = al termino
+    
+    return resp.json({ //IMPORTANTE: poner el return para que no siga ejecutando nda de la funcion
+        results: usuarios
+    });
 }
 
 const buscar = (req, resp = response) => {
